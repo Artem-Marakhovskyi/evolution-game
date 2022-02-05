075f925f51f651a19b10efc82c3ffb40c9cfaee1
Artem Marakhovskyi <artem.marakhovskyi@gmail.com>
2022-02-05 16:04:27 +0200

    skeleton is ready

--- cross_cutting/test/cross_cutting_test.dart
+++ cross_cutting/test/cross_cutting_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
+    final awesome = CrossCutting();
 
     setUp(() {
       // Additional setup goes here.
